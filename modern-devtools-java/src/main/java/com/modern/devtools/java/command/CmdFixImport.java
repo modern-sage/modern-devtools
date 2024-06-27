37a71537f88c0005120a49f444e469157e2f9b0a
zhangjun <brucezhang_jjz@163.com>
2024-06-27 19:19:06 +0800

    import fix

---
+++ modern-devtools-java/src/main/java/com/modern/devtools/java/command/CmdFixImport.java
@@ -0,0 +1,130 @@
+
+package com.modern.devtools.java.command;
+
+import com.modern.devtools.java.Command;
+import com.modern.devtools.java.JavaFile;
+import com.modernframework.core.utils.CollectionUtils;
+import com.modernframework.core.utils.StringUtils;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * CmdInfoJava
+ *
+ * @author <a href="mailto:brucezhang_jjz@163.com">zhangjun</a>
+ * @since 1.0.0
+ */
+public class CmdFixImport implements CmdExecutor {
+
+    @Override
+    public String supportCommand() {
+        return Command.FIX_IMPORT.getKey();
+    }
+
+    @Override
+    public void execute(String command) {
+        Map<String, List<JavaFile>> javaFileMap = getContext().getJavaFileMap();
+        AtomicInteger fixNum = new AtomicInteger();
+        javaFileMap.values().forEach(x -> x.forEach(y -> {
+            File file = y.getFile();
+            StringBuilder javaContent = new StringBuilder();
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    javaContent.append(line).append("\n");
+                }
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+
+            String[] contents = javaContent.toString().split("\n");
+            List<String> sourceImports = new LinkedList<>();
+            for (String content : contents) {
+                if (content.startsWith("import ")) {
+                    sourceImports.add(content);
+                }
+            }
+
+            if (CollectionUtils.isEmpty(sourceImports)) {
+                System.err.printf("文件 %s 缺少 import 信息%n", y.getJavaName());
+            } else {
+                try {
+                    Map<String, String> replaceMap = new HashMap<>();
+                    sourceImports.forEach(s -> {
+                        String importText;
+                        String simpleJavaName;
+                        if (s.startsWith("import static ")) {
+                            importText = s.substring("import static ".length(), s.lastIndexOf(";")).trim();
+                            importText = importText.substring(0, importText.lastIndexOf("."));
+                            simpleJavaName = importText.substring(importText.lastIndexOf(".") + 1);
+                        } else {
+                            importText = s.substring("import ".length(), s.lastIndexOf(";")).trim();
+                            simpleJavaName = importText.substring(importText.lastIndexOf(".") + 1);
+                        }
+
+                        if (javaFileMap.containsKey(simpleJavaName)) {
+                            List<JavaFile> matchJavaFiles = javaFileMap.get(simpleJavaName);
+                            boolean matched = false;
+                            for (JavaFile matchJavaFile : matchJavaFiles) {
+                                String matchJavaFilePath = matchJavaFile.getJavaName();
+                                if (matchJavaFilePath.equals(importText)) {
+                                    matched = true;
+                                    break;
+                                }
+                            }
+                            if (!matched) {
+                                JavaFile matchJavaFile = null;
+                                if (matchJavaFiles.size() > 1) {
+                                    boolean chose = false;
+                                    while (!chose) {
+                                        try {
+                                            System.out.println("匹配到多个文件，请选择其中一个用来修复 \n");
+                                            for (int i = 0; i < matchJavaFiles.size(); i++) {
+                                                System.out.printf("%d - %s%n", i, matchJavaFiles.get(i).getJavaName());
+                                            }
+                                            String choose = getContext().getScanner().nextLine().trim();
+                                            int chooseNum = Integer.parseInt(choose);
+                                            matchJavaFile = matchJavaFiles.get(chooseNum);
+                                            chose = true;
+                                        } catch (Throwable e) {
+                                            chose = false;
+                                        }
+                                    }
+                                } else {
+                                    matchJavaFile = matchJavaFiles.get(0);
+                                }
+                                String targetImport = "import " + matchJavaFile.getJavaName() + ";";
+                                replaceMap.put(s, targetImport);
+                            }
+                        }
+
+                        if(!replaceMap.isEmpty()) {
+                            fixNum.addAndGet(1);
+                            System.out.printf("修复 %s import 信息%n", y.getJavaName());
+                            String targetContext = javaContent.toString();
+                            for (String key : replaceMap.keySet()) {
+                                String target = replaceMap.get(key);
+                                targetContext = targetContext.replace(key, target);
+                                System.out.printf("%s -> %s%n", key, target);
+                            }
+                            try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+                                bw.write(targetContext);
+                                bw.flush();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    });
+                } catch (StringIndexOutOfBoundsException e) {
+                    e.printStackTrace();
+                }
+            }
+        }));
+        System.out.printf("修复完成，修复文件数: %s%n", fixNum.get());
+    }
+}
